b18cd2824e684cb03ecb8731052080818f9e6909
Aniket Chate <chateaniket5@gmail.com>
2023-05-02 16:09:30 +0530

    commit

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+import DashboardLayout from './layouts/dashboard';
+
+
+import DashboardAppPage from './pages/DashboardAppPage';
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+  const routes = useRoutes([
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/app" />, index: true },
+        { path: 'app', element: <DashboardAppPage /> },
+
+      ],
+    },
+    
+  ]);
+
+  return routes;
+}
